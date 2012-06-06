1d3d6c7457b2e12a8069df6915a5a59ac19d9e4f
Evan Hahn <me@evanhahn.com>
2012-06-06 14:26:03 -0700

    Reorganize git aliases, add a git add typo

--- go.sh
+++ go.sh
@@ -312,10 +312,13 @@ git config --global core.editor "vim"
 # Git aliases
 git config --global alias.unadd "reset HEAD"
 git config --global alias.s "status"
+git config --global alias.d "diff"
+
+# Git typo aliases
+git config --glboal alias.ad "add"
 git config --global alias.statu "status"
 git config --global alias.stat "status"
 git config --global alias.sttus "status"
-git config --global alias.d "diff"
 git config --global alias.dif "diff"
 
 # Legit (git-legit.org)
