bc636c0444ac12aa5fdf66e2a2772acc54011ccd
Evan Hahn <me@evanhahn.com>
2012-08-17 23:05:36 -1000

    Whoops, symlinks fixed

--- script/symlink.sh
+++ script/symlink.sh
@@ -20,46 +20,46 @@ fi
 # Vim (.vimrc and .vim directory)
 rm $HOME/.vimrc &> /dev/null
 rm -r $HOME/.vim &> /dev/null
-ln -s $PWD/resources/vimrc $HOME/.vimrc &> /dev/null
-ln -s $PWD/resources/vim $HOME/.vim &> /dev/null
+ln -s $PWD/../resources/vimrc $HOME/.vimrc &> /dev/null
+ln -s $PWD/../resources/vim $HOME/.vim &> /dev/null
 
 # Nano (.nanorc and .nano directory)
 rm $HOME/.nanorc &> /dev/null
 rm -r $HOME/.nano &> /dev/null
-ln -s $PWD/resources/nanorc $HOME/.nanorc &> /dev/null
-ln -s $PWD/resources/nano $HOME/.nano &> /dev/null
+ln -s $PWD/../resources/nanorc $HOME/.nanorc &> /dev/null
+ln -s $PWD/../resources/nano $HOME/.nano &> /dev/null
 
 # SSH
 rm $HOME/.ssh/config &> /dev/null
-ln -s $PWD/resources/sshconfig $HOME/.ssh/config &> /dev/null
+ln -s $PWD/../resources/sshconfig $HOME/.ssh/config &> /dev/null
 
 # Git
 rm $HOME/.gitconfig &> /dev/null
-ln -s $PWD/resources/gitconfig $HOME/.gitconfig &> /dev/null
+ln -s $PWD/../resources/gitconfig $HOME/.gitconfig &> /dev/null
 
 # Input aliases
 rm $HOME/.inputrc &> /dev/null
-ln -s $PWD/resources/inputrc $HOME/.inputrc &> /dev/null
+ln -s $PWD/../resources/inputrc $HOME/.inputrc &> /dev/null
 
 # csh
 rm $HOME/.cshrc &> /dev/null
-ln -s $PWD/resources/cshrc $HOME/.cshrc
+ln -s $PWD/../resources/cshrc $HOME/.cshrc
 
 # screen
 rm $HOME/.screenrc &> /dev/null
-ln -s $PWD/resources/screenrc $HOME/.screenrc
+ln -s $PWD/../resources/screenrc $HOME/.screenrc
 
 # IRB
 rm $HOME/.irbrc &> /dev/null
-ln -s $PWD/resources/irbrc $HOME/.irbrc
+ln -s $PWD/../resources/irbrc $HOME/.irbrc
 
 # basrc or bash_profile
 # =====================
 
 if [[ $PLATFORM == "OSX" ]]; then
 	rm $HOME/.bash_profile &> /dev/null
-	ln -s $PWD/resources/bashrc $HOME/.bash_profile
+	ln -s $PWD/../resources/bashrc $HOME/.bash_profile
 else
 	rm $HOME/.bashrc &> /dev/null
-	ln -s $PWD/resources/bashrc $HOME/.bashrc
+	ln -s $PWD/../resources/bashrc $HOME/.bashrc
 fi
