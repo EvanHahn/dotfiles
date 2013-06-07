6b7db86138f057a690d7563b85afbbec0d59f6b6
Evan Hahn <me@evanhahn.com>
2013-06-07 15:44:18 -0700

    import some linux-only settings from bashrc

---
+++ resources/zsh/linux.zsh
@@ -0,0 +1,11 @@
+alias e="gvim"
+
+alias pingo="ping -c 1 74.125.225.36 | grep -v transmitted | grep time"
+
+alias mate="gvim"
+
+alias open="gnome-open"
+
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
+
