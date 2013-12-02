a554dbb44e7dc9e21d5335799364b7b531abe753
Evan Hahn <me@evanhahn.com>
2013-12-01 19:26:28 -0500

    syntax highlighting for zsh
    might remove it bc it's a bit slow...

---
+++ resources/zsh/highlighting.zsh
@@ -0,0 +1,13 @@
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+
+ZSH_HIGHLIGHT_STYLES[default]=none
+
+ZSH_HIGHLIGHT_STYLES[alias]=fg=001,bold
+ZSH_HIGHLIGHT_STYLES[command]=fg=001,bold
+ZSH_HIGHLIGHT_STYLES[builtin]=fg=001,bold
+ZSH_HIGHLIGHT_STYLES[function]=fg=001,bold
+
+ZSH_HIGHLIGHT_STYLES[precommand]=fg=underline
+ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=underline
