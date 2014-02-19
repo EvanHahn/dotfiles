7973da04c38f0ecc6a412b1de0479525dda4bdf2
Evan Hahn <me@evanhahn.com>
2014-02-18 19:11:22 -0500

    spacing between xmonad windows

--- resources/xmonad/xmonad.hs
+++ resources/xmonad/xmonad.hs
@@ -1,12 +1,27 @@
 import XMonad
+import XMonad.Layout.Spacing
+import XMonad.Layout.ResizableTile
 import System.Exit
 
 main = xmonad defaults
 
+myLayoutHook = tiled ||| Mirror tiled
+	where
+		tiled = spacing 1 $ ResizableTall nmaster delta ratio []
+		nmaster = 1
+		ratio = 1/2
+		delta = 1/100
+
 defaults = defaultConfig {
+
 	terminal = "xterm",
-	borderWidth = 10,
-	normalBorderColor = "#000000",
-	focusedBorderColor = "#ffffff",
+
+	borderWidth = 1,
+	normalBorderColor = "#666666",
+	focusedBorderColor = "#cbcbcb",
+
+	layoutHook = myLayoutHook,
+
 	focusFollowsMouse = False
+
 }
