5853588b9fd0b8618ed2d42e3f3e4f2a6aa93afe
Evan Hahn <me@evanhahn.com>
2014-06-19 11:06:37 -0700

    pink now

--- resources/zsh/prompt.zsh
+++ resources/zsh/prompt.zsh
@@ -10,8 +10,8 @@ for color in {000..255}; do
 	BG[$color]="[48;5;${color}m"
 done
 
-PROMPT='
-%U%{$fg[red]%}%~%u %{$FG[240]%}%n@%m %{$reset_color%}
-'
+PROMPT="
+%U%{$FG[162]%}%~%u %{$FG[240]%}%n@%m %{$reset_color%}
+"
 
-SPROMPT='%{$FG[240]%}When you said %{$FG[230]%}%R%{$FG[240]%}, did you mean %{$FG[228]%}%r%{$FG[240]%}?%{$reset_color%} '
+SPROMPT="%{$FG[240]%}When you said %{$FG[230]%}%R%{$FG[240]%}, did you mean %{$FG[228]%}%r%{$FG[240]%}?%{$reset_color%} "
