bcbb06c10c19ed387cd1a987d7ea6679685bacb1
Evan Hahn <me@evanhahn.com>
2014-06-19 11:15:23 -0700

    even pinker now

--- resources/zsh/highlighting.zsh
+++ resources/zsh/highlighting.zsh
@@ -4,10 +4,10 @@ ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
 
 ZSH_HIGHLIGHT_STYLES[default]=none
 
-ZSH_HIGHLIGHT_STYLES[alias]=fg=001,bold
-ZSH_HIGHLIGHT_STYLES[command]=fg=001,bold
-ZSH_HIGHLIGHT_STYLES[builtin]=fg=001,bold
-ZSH_HIGHLIGHT_STYLES[function]=fg=001,bold
+ZSH_HIGHLIGHT_STYLES[alias]=fg=005,bold
+ZSH_HIGHLIGHT_STYLES[command]=fg=005,bold
+ZSH_HIGHLIGHT_STYLES[builtin]=fg=005,bold
+ZSH_HIGHLIGHT_STYLES[function]=fg=005,bold
 
 ZSH_HIGHLIGHT_STYLES[precommand]=fg=underline
 ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=underline
