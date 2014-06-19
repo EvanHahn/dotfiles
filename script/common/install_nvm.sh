e7915a843d980363982fa782153b85edc784c3cf
Evan Hahn <me@evanhahn.com>
2014-06-19 23:49:02 +0000

    i didn't need that fancy piping after all

--- script/common/install_nvm.sh
+++ script/common/install_nvm.sh
@@ -7,9 +7,6 @@ curl -sSL https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | P
 # start nvm
 source $HOME/.nvm/nvm.sh
 
-# install the latest 0.10
-NODE_VERSION_TO_INSTALL=`nvm ls-remote | grep "v0\.10\." | tail -n 1`
-
-# install and default that
-nvm install $NODE_VERSION_TO_INSTALL
-nvm alias default $NODE_VERSION_TO_INSTALL
+# 0.10 please
+nvm install 0.10
+nvm alias default 0.10
