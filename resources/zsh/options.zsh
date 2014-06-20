4f96f0eb4f4b8a6400e1caee76d9933cb78324d3
Evan Hahn <me@evanhahn.com>
2014-06-20 00:11:25 +0000

    ls colors on the linux

--- resources/zsh/options.zsh
+++ resources/zsh/options.zsh
@@ -3,7 +3,7 @@ export USE_EDITOR=$EDITOR
 export VISUAL=$EDITOR
 
 export CLICOLOR=1
-# TODO: LS_COLORS for Linux
+export LS_COLORS="di=30;46:tw=30;46:ow=30;46:ex=31:su=31:sg=31:"
 export LSCOLORS="agxxxxxxbxxxxxbxbxagag"
 
 export GREP_OPTIONS='--color=auto'
