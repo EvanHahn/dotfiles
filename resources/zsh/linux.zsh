9d971d26772300e990f378c8758aadf4e9c7beda
Evan Hahn <me@evanhahn.com>
2014-06-20 00:11:33 +0000

    move some aliases around for linux

--- resources/zsh/linux.zsh
+++ resources/zsh/linux.zsh
@@ -1,12 +1,13 @@
+alias open="gnome-open"
+alias pbcopy="xsel --clipboard --input"
+alias pbpaste="xsel --clipboard --output"
+
+alias ls="ls --color"
+
 alias e="gvim"
 
 alias pingo="ping -c 1 74.125.225.36 | grep -v transmitted | grep time"
 
 alias mate="gvim"
-
-alias open="gnome-open"
-
-alias pbcopy="xsel --clipboard --input"
-alias pbpaste="xsel --clipboard --output"
 
 alias reboot="sudo reboot"
