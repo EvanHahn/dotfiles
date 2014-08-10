2b04e302d735139235ec754dc71a847dc259e8b8
Evan Hahn <me@evanhahn.com>
2014-08-10 11:47:04 -1000

    ff = firefox on osx

--- resources/zsh/osx.zsh
+++ resources/zsh/osx.zsh
@@ -19,6 +19,7 @@ alias sleepybear="osascript -e 'tell application \"System Events\" to sleep'"
 
 alias e="open -a MacVim"
 alias gitx="open -a GitX ."
+alias ff="open -a Firefox"
 
 alias pingo="ping -o 74.125.225.36 | grep time"
 alias flushdns="sudo killall -HUP mDNSResponder"
