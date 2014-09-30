08d5c7121403366b16ed7f4dd135d664692061b1
Evan Hahn <me@evanhahn.com>
2014-09-30 15:32:38 -0400

    get "direction" file is going

--- iscp.sh
+++ iscp.sh
@@ -4,3 +4,10 @@ set -e
 echo -en '\033[35m'
 echo "~~** hey it's me, interactive scp **~~"
 echo -en '\033[0m'
+
+direction=''
+while [[ "$direction" != 'from' ]] && [[ "$direction" != 'to' ]]; do
+  echo -n 'Are you copying FROM or TO a remote server? '
+  read temp
+  direction="$(echo $temp | awk '{print tolower($0)}')"
+done
