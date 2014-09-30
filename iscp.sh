ed8387d17ff2f47d9c2eba51cdf966bbff46c09c
Evan Hahn <me@evanhahn.com>
2014-09-30 15:34:50 -0400

    remove some []

--- iscp.sh
+++ iscp.sh
@@ -5,8 +5,8 @@ echo -en '\033[35m'
 echo "~~** hey it's me, interactive scp **~~"
 echo -en '\033[0m'
 
-direction=''
-while [[ "$direction" != 'from' ]] && [[ "$direction" != 'to' ]]; do
+direction='not yet specified'
+while [ "$direction" != 'from' ] && [ "$direction" != 'to' ]; do
   echo -n 'Are you copying FROM or TO a remote server? '
   read temp
   direction="$(echo $temp | awk '{print tolower($0)}')"
