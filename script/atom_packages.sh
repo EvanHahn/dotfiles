1ddd68d98de925a4f0297beb16241afed10162b0
Evan Hahn <me@evanhahn.com>
2014-10-30 12:00:56 -0400

    Script to install Atom packages

---
+++ script/atom_packages.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+packages=(
+language-csharp
+language-jade
+language-svg
+language-typescript
+language-verilog
+language-viml
+
+linter
+linter-coffeelint
+linter-flake8
+linter-jshint
+
+sort-lines
+git-plus
+)
+
+for package in "${packages[@]}"; do
+   apm install "$package"
+done
