588c4676bc8a94da74872ff70ff7b81e4d8a10df
Evan Hahn <me@evanhahn.com>
2014-11-10 08:15:55 -0500

    Install trash on your own

--- resources/zsh/osx.zsh
+++ resources/zsh/osx.zsh
@@ -1,20 +1,3 @@
-function trash() {
-	local trash_dir="${HOME}/.Trash"
-	local temp_ifs=$IFS
-	IFS=$'\n'
-	for item in "$@"; do
-		if [[ -e "$item" ]]; then
-			item_name="$(basename $item)"
-			if [[ -e "${trash_dir}/${item_name}" ]]; then
-				mv -f "$item" "${trash_dir}/${item_name} $(date "+%H-%M-%S")"
-			else
-				mv -f "$item" "${trash_dir}/"
-			fi
-		fi
-	done
-	IFS=$temp_ifs
-}
-
 source `brew --prefix`/etc/profile.d/z.sh
 
 alias sleepybear="osascript -e 'tell application \"System Events\" to sleep'"
