2ebbcfc318b93d61bf7cee3551aed27b26584811
Evan Hahn <me@evanhahn.com>
2014-12-01 13:48:25 -0500

    beef up symlink with quotes

--- script/symlink.sh
+++ script/symlink.sh
@@ -3,15 +3,15 @@ set -e
 set -u
 
 symlink() {
-  local source_file=$1
-  local destination_file=$2
-  if [ -e $destination_file ]; then
-    rm -r $destination_file
+  local source_file="$1"
+  local destination_file="$2"
+  if [ -e "$destination_file" ]; then
+    rm -r "$destination_file"
   fi
-  ln -s $source_file $destination_file
+  ln -s "$source_file" "$destination_file"
 }
 
-mkdir -p $HOME/.config
+mkdir -p "$HOME/.config"
 
 scripts_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 resources_directory="$scripts_directory/../resources"
@@ -25,9 +25,9 @@ for file in $(ls $resources_directory); do
   fi
 done
 
-mkdir -p $HOME/.ssh
-symlink "$resources_directory/sshconfig" $HOME/.ssh/config
+mkdir -p "$HOME/.ssh"
+symlink "$resources_directory/sshconfig" "$HOME/.ssh/config"
 
 # make these history files nothing
-symlink /dev/null $HOME/.lesshst
-symlink /dev/null $HOME/.coffee_history
+symlink /dev/null "$HOME/.lesshst"
+symlink /dev/null "$HOME/.coffee_history"
