791cc79efb4b5a39bb7f48b94562adf26075c26c
Evan Hahn <me@evanhahn.com>
2015-01-06 22:03:49 -0600

    steal @adamyonk's brilliant script
    https://github.com/adamyonk/dotfiles/blob/master/script/install_brews

---
+++ script/osx-packages.rb
@@ -0,0 +1,117 @@
+#!/usr/bin/env ruby
+
+# what are we installing
+
+TAPS = [
+  'homebrew/games',
+  'caskroom/fonts'
+]
+
+BREWS = [
+  # replacements
+  'bash',
+  'vim',
+  'zsh',
+  'git',
+
+  # other CLI
+  'aspell --with-lang-en',
+  'ffmpeg --with-faac --with-fdk-aac --with-ffplay --with-fontconfig --with-freetype --with-frei0r --with-libass --with-libbluray --with-libcaca --with-libquvi --with-libsoxr --with-libvidstab --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-openssl --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-x265',
+  'lua',
+  'lynx',
+  'mobile-shell',
+  'most',
+  'naga',
+  'pianobar',
+  'reattach-to-user-namespace',
+  'ruby',
+  'spoof-mac',
+  'tmux',
+  'trash',
+  'vitetris',
+  'youtube-dl'
+]
+
+CASKS = [
+
+  'alfred',
+  'atom',
+  'brightness',
+  'chromium',
+  'cloud',
+  'dash',
+  'dropbox',
+  'firefox',
+  'flux',
+  'gitx',
+  'handbrake',
+  'iterm2',
+  'macdown',
+  'openemu',
+  'paintbrush',
+  'proxpn',
+  'rdio',
+  'skype',
+  'spectacle',
+  'spideroak',
+  'the-unarchiver',
+  'vlc',
+
+  # fonts
+  'font-lato',
+  'font-open-sans',
+  'font-source-code-pro',
+
+  # quicklook plugins
+  'qlcolorcode',
+  'qlmarkdown',
+  'qlstephen',
+  'quicklook-csv',
+  'quicklook-json',
+  'webpquicklook'
+
+]
+
+GEMS = [
+  'delicious-cli',
+  'rdio-cli'
+]
+
+# do the installation
+
+brew_path = `which brew`
+if brew_path == ''
+  puts 'please install Homebrew from http://brew.sh'
+  exit 1
+end
+
+INSTALLED_BREWS = `brew list`.split("\n")
+INSTALLED_CASKS = `brew cask list`.split("\n")
+INSTALLED_GEMS = `gem list --no-versions`.split("\n")
+INSTALLED_TAPS = `brew tap`.split("\n")
+
+$command_queue = ['brew update']
+unless INSTALLED_BREWS.include? 'brew-cask'
+  $command_queue.push "brew install caskroom/cask/brew-cask"
+end
+
+def install(list, command, existing)
+  list.each do |package|
+    name = package.split.first
+    unless existing.include? name
+      print "install #{name}? "
+      skip_cmd = gets.strip
+      go_ahead = (skip_cmd == 'y') || (skip_cmd == 'yes') || (skip_cmd == '')
+      $command_queue.push "#{command} #{package}" if go_ahead
+    end
+  end
+end
+
+install TAPS, "brew tap", INSTALLED_TAPS
+install BREWS, "brew install", INSTALLED_BREWS
+install CASKS, "brew cask install", INSTALLED_CASKS
+install GEMS, "gem install", INSTALLED_GEMS
+
+$command_queue.each do |command|
+  system command
+end
