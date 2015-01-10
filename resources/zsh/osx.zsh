05168d48236423d82440ee685f2aab4f5cd88799
Evan Hahn <me@evanhahn.com>
2015-01-10 15:59:37 -0800

    Remove old Z and NVM stuff

--- resources/zsh/osx.zsh
+++ resources/zsh/osx.zsh
@@ -8,14 +8,3 @@ alias gitx='open -a GitX .'
 alias flushdns='sudo killall -HUP mDNSResponder'
 
 sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent' # flush downloads list
-
-z_path=`brew --prefix`/etc/profile.d/z.sh
-if [ -e "$z_path" ]; then
-  source "$z_path"
-fi
-
-nvm_path=$(brew --prefix nvm)/nvm.sh
-if [ -e "$nvm_path" ]; then
-  export NVM_DIR=~/.nvm
-  source "$nvm_path"
-fi
