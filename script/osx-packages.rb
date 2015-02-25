4303b464f5cb520379c8c5991535d2af0917495c
Evan Hahn <me@evanhahn.com>
2015-02-25 11:04:49 -0600

    brew install colordiff

--- script/osx-packages.rb
+++ script/osx-packages.rb
@@ -17,6 +17,7 @@ BREWS = [
 
   # other CLI
   'aspell --with-lang-en',
+  'colordiff',
   'dict',
   'ffmpeg --with-faac --with-fdk-aac --with-ffplay --with-fontconfig --with-freetype --with-frei0r --with-libass --with-libbluray --with-libcaca --with-libquvi --with-libsoxr --with-libvidstab --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-openssl --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-x265',
   'lua',
