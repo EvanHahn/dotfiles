cc4e4699f0d600ee449267df6e71672ac00196dc
Evan Hahn <me@evanhahn.com>
2015-04-22 17:27:11 -0500

    Change prompt color on Vagrant machines

--- resources/zsh/prompt.zsh
+++ resources/zsh/prompt.zsh
@@ -10,8 +10,14 @@ for color in {000..255}; do
 	BG[$color]="[48;5;${color}m"
 done
 
+if [ "$(whoami)" = 'vagrant' ]; then
+  prompt_primary_color=117
+else
+  prompt_primary_color=162
+fi
+
 PROMPT="
-%U%{$FG[162]%}%~%u %{$FG[240]%}%n@%m %{$reset_color%}
+%U%{$FG[$prompt_primary_color]%}%~%u %{$FG[240]%}%n@%m %{$reset_color%}
 "
 
 SPROMPT="%{$FG[240]%}When you said %{$FG[230]%}%R%{$FG[240]%}, did you mean %{$FG[228]%}%r%{$FG[240]%}?%{$reset_color%} "
