6b1f33c645ec02769cbf6d83d8e2e4c1177a9b9e
Evan Hahn <me@evanhahn.com>
2015-05-01 15:57:25 -0500

    change ZSH prompt

--- resources/zsh/prompt.zsh
+++ resources/zsh/prompt.zsh
@@ -1,7 +1,6 @@
 autoload colors; colors # enable colors
 
 setopt prompt_subst # enable hecka stuff
-
 setopt transient_rprompt # show rprompt on current prompt only
 
 typeset -Ag FG BG
@@ -10,14 +9,17 @@ for color in {000..255}; do
 	BG[$color]="[48;5;${color}m"
 done
 
-if [ "$(whoami)" = 'vagrant' ]; then
-  prompt_primary_color=117
-else
-  prompt_primary_color=162
-fi
+_prompt_status_color() {
+  if [[ "$?" == '0' ]]; then
+    echo -e "$FG[240]"
+  else
+    echo -e "$FG[160]"
+  fi
+}
 
-PROMPT="
-%U%{$FG[$prompt_primary_color]%}%~%u %{$FG[240]%}%n@%m %{$reset_color%}
-"
+PROMPT='
+ %{$(_prompt_status_color)%}>%{$reset_color%} '
 
-SPROMPT="%{$FG[240]%}When you said %{$FG[230]%}%R%{$FG[240]%}, did you mean %{$FG[228]%}%r%{$FG[240]%}?%{$reset_color%} "
+RPROMPT='%{$FG[163]%}%~%u%{$reset_color%} %{$(_prompt_status_color)%}<%{$reset_color%} '
+
+SPROMPT="%{$FG[240]%}when you said %{$FG[230]%}%R%{$FG[240]%}, did you mean %{$FG[228]%}%r%{$FG[240]%}?%{$reset_color%} "
