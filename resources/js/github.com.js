2384a63b9df80af32ccf6f7687ecd836ea48bf9e
Evan Hahn <me@evanhahn.com>
2015-05-09 10:31:31 -0500

    Fix github login bug

--- resources/js/github.com.js
+++ resources/js/github.com.js
@@ -1,3 +1,6 @@
-if (window.location.href === 'https://github.com/') {
+var isHomepage = window.location.href === 'https://github.com/';
+var isLoggedOut = document.body.classList.contains('logged_out');
+
+if (isHomepage && isLoggedOut) {
   window.location.href = 'https://github.com/login';
 }
