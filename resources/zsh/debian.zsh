dee66ea8f00290edf8cb6eedca442870490c7930
Evan Hahn <me@evanhahn.com>
2015-05-24 23:14:05 -0700

    no more linuxbrew

--- resources/zsh/debian.zsh
+++
@@ -1,7 +0,0 @@
-export PATH="$HOME/.linuxbrew/bin:$PATH"
-export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
-export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
-
-alias open='xdg-open'
-
-alias ls='ls --color'
---
+++ resources/zsh/ubuntu.zsh
@@ -0,0 +1,3 @@
+alias open='xdg-open'
+
+alias ls='ls --color'
