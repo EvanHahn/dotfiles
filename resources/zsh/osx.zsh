cc7d2dd6d9401b3c497b7f42651d634d16677c90
Evan Hahn <me@evanhahn.com>
2015-11-19 12:54:20 -0800

    move flushdns to a bin

--- resources/zsh/osx.zsh
+++ resources/zsh/osx.zsh
@@ -5,8 +5,6 @@ unset DISPLAY
 
 sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent' # flush downloads list
 
-alias flushdns='sudo killall -HUP mDNSResponder'
-
 alias shred='srm'
 
 alias gitx='open -a GitX .'
