fa9f1c3b24502c4ea10a846c9dea84827134b647
Evan Hahn <me@evanhahn.com>
2016-02-24 13:12:25 -0800

    rework some Phoenix configuration

--- resources/phoenix.js
+++ resources/phoenix.js
@@ -1,57 +1,77 @@
+/* global _, Phoenix, Window */
+
 // utilities
 // ---------
 
-function log() {
-  var arg, notify;
+function log () {
+  var arg, notify
   for (var i = 0; i < arguments.length; i++) {
-    arg = arguments[i];
+    arg = arguments[i]
     if (_.isString(arg)) {
-      notify = arg;
+      notify = arg
     } else if (_.isArray(arg)) {
-      notify = arg.join(',');
+      notify = arg.join(',')
     } else {
-      notify = JSON.stringify(arg);
+      notify = JSON.stringify(arg)
     }
-    Phoenix.notify(notify);
+    Phoenix.notify(notify)
   }
 }
 
 // window moving utilities
 // =======================
 
-function half(window) {
-  var screenFrame = window.screen().frameInRectangle();
+function full (window) {
+  var screenFrame = window.screen().frameInRectangle()
+  window.setSize({
+    width: screenFrame.width,
+    height: screenFrame.height
+  })
+}
+
+function half (window) {
+  var screenFrame = window.screen().frameInRectangle()
   window.setSize({
     width: screenFrame.width / 2,
     height: screenFrame.height
-  });
+  })
 }
 
-function snapToLeft(window) {
-  window.setTopLeft({ x: 0, y: 0 });
-  half(window);
+function snapToLeft (window) {
+  window.setTopLeft({ x: 0, y: 0 })
+  half(window)
 }
 
-function snapToRight(window) {
-  var screenFrame = window.screen().frameInRectangle();
-  window.setTopLeft({ x: screenFrame.width / 2, y: 0 });
-  half(window);
+function snapToRight (window) {
+  var screenFrame = window.screen().frameInRectangle()
+  window.setTopLeft({ x: screenFrame.width / 2, y: 0 })
+  half(window)
+}
+
+function makeFullscreen (window) {
+  window.setTopLeft({ x: 0, y: 0 })
+  full(window)
 }
 
 // real commands
 // =============
 
-function snapCurrentToLeft() {
-  snapToLeft(Window.focusedWindow());
+function snapCurrentToLeft () {
+  snapToLeft(Window.focusedWindow())
 }
 
-function snapCurrentToRight() {
-  snapToRight(Window.focusedWindow());
+function snapCurrentToRight () {
+  snapToRight(Window.focusedWindow())
 }
 
-var keys = [
+function makeCurrentFullscreen () {
+  makeFullscreen(Window.focusedWindow())
+}
+
+var keys = [  // eslint-disable-line no-unused-vars
   Phoenix.bind('left', ['alt', 'cmd'], snapCurrentToLeft),
   Phoenix.bind('right', ['alt', 'cmd'], snapCurrentToRight),
-];
+  Phoenix.bind('up', ['alt', 'cmd'], makeCurrentFullscreen)
+]
 
-log('Phoenix loaded');
+log('Phoenix loaded')
