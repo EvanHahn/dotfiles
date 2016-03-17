43a0eddcb7e5749a7c0ea9bd5438da3812e78adf
Evan Hahn <me@evanhahn.com>
2016-03-17 11:20:25 -0700

    Pocket Casts back/forward keyboard shortcuts

--- resources/js/play.pocketcasts.com.js
+++ resources/js/play.pocketcasts.com.js
@@ -1,3 +1,11 @@
 if (document.getElementById('start_page')) {
-  location.href = '/web';
+  location.href = '/web'
 }
+
+$(document.documentElement).on('keypress', function (event) {
+  if (event.keyCode === 37) {
+    $('.skip_back_button').click()
+  } else if (event.keyCode === 39) {
+    $('.skip_forward_button').click()
+  }
+})
