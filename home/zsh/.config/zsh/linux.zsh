99359cb7614082b97ff224dacc53ba158be0e578
Evan Hahn <me@evanhahn.com>
2016-10-09 14:46:29 -0700

    More XDG_ variables

--- home/zsh/.config/zsh/linux.zsh
+++ home/zsh/.config/zsh/linux.zsh
@@ -1,1 +1,9 @@
+export XDG_DESKTOP_DIR="$HOME/desktop"
+export XDG_DOCUMENTS_DIR="$HOME/documents"
+export XDG_DOWNLOAD_DIR="$HOME/desktop"
+export XDG_MUSIC_DIR="$HOME/music"
+export XDG_PICTURES_DIR="$HOME/pictures"
+export XDG_PUBLICSHARE_DIR="$HOME/public"
+export XDG_VIDEOS_DIR="$HOME/videos"
+
 alias ls='ls --color'
--- home/zsh/.config/zsh/osx.zsh
+++ home/zsh/.config/zsh/osx.zsh
@@ -1,3 +1,11 @@
+export XDG_DESKTOP_DIR="$HOME/Desktop"
+export XDG_DOCUMENTS_DIR="$HOME/Documents"
+export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+export XDG_MUSIC_DIR="$HOME/Music"
+export XDG_PICTURES_DIR="$HOME/Pictures"
+export XDG_PUBLICSHARE_DIR="$HOME/Public"
+export XDG_VIDEOS_DIR="$HOME/Videos"
+
 export BROWSER=open
 unset DISPLAY
 
