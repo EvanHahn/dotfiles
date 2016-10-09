c9446d2a9e81ead804ed71bb2e6eedf684b44d59
Evan Hahn <me@evanhahn.com>
2016-10-09 11:58:37 -0700

    gvim alias should only be on macOS

--- home/zsh/.config/zsh/osx.zsh
+++ home/zsh/.config/zsh/osx.zsh
@@ -2,3 +2,5 @@ export BROWSER=open
 unset DISPLAY
 
 sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent' # flush downloads list
+
+alias gvim=mvim
