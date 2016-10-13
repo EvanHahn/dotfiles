8fff1f2906680ba7b9922531e2f01e017e9f7625
Evan Hahn <me@evanhahn.com>
2016-10-12 20:23:25 -0700

    install Z on arch

--- home/zsh/.config/zsh/start_z.sh
+++ home/zsh/.config/zsh/start_z.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
-z_path='/a/bogus/path'
-
 if [[ "$(uname -s)" = 'Darwin' ]]; then
   z_path="$(brew --prefix)/etc/profile.d/z.sh"
+else
+  z_path='/usr/share/z/z.sh'
 fi
 
 if [[ -e "$z_path" ]]; then
