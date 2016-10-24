42f0ad7de622d67cce02601d6bd64b3dc8d4af03
Evan Hahn <me@evanhahn.com>
2016-10-23 23:12:29 -0700

    begin atom config

--- home/zsh/.config/zsh/env.zsh
+++ home/zsh/.config/zsh/env.zsh
@@ -70,6 +70,8 @@ export LSCOLORS='afxxxxxxbxxxxxbxbxagag'
 
 export LYNX_CFG="$XDG_CONFIG_HOME/lynx/lynx.cfg"
 
+export ATOM_HOME="$XDG_CONFIG_HOME/atom"
+
 export NODE_REPL_HISTORY="$XDG_CACHE_HOME/node_repl_history"
 export NPM_CONFIG_EDITOR="$EDITOR"
 export NPM_CONFIG_INIT_AUTHOR_NAME='Evan Hahn'
