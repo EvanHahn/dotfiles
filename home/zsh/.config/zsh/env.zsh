99be2e405811fb079a5c902389da451d1755734b
Evan Hahn <me@evanhahn.com>
2016-12-05 13:33:06 -0800

    alphabetize environment variables

--- home/zsh/.config/zsh/env.zsh
+++ home/zsh/.config/zsh/env.zsh
@@ -53,6 +53,13 @@ export CLICOLOR=1
 
 # commands
 
+export ATOM_HOME="$XDG_CONFIG_HOME/atom"
+
+export bower_storage__packages="$XDG_CACHE_HOME/bower/packages"
+export bower_storage__registry="$XDG_CACHE_HOME/bower/registry"
+export bower_storage__links="$XDG_CACHE_HOME/bower/links"
+export bower_tmp="$XDG_CACHE_HOME/bower/tmp"
+
 export GREP_COLOR='00;36'
 
 export HOMEBREW_NO_EMOJI=1
@@ -61,16 +68,12 @@ export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSECURE_REDIRECT=1
 
-if hash less 2>/dev/null; then
-  export LESS='--ignore-case --long-prompt --QUIET --raw-control-chars --no-init'
-fi
+export LESS='--ignore-case --long-prompt --QUIET --raw-control-chars --no-init'
 
 export LS_COLORS='di=30;46:tw=30;46:ow=30;46:ex=31:su=31:sg=31:'
 export LSCOLORS='afxxxxxxbxxxxxbxbxagag'
 
 export LYNX_CFG="$XDG_CONFIG_HOME/lynx/lynx.cfg"
-
-export ATOM_HOME="$XDG_CONFIG_HOME/atom"
 
 export NODE_REPL_HISTORY="$XDG_CACHE_HOME/node_repl_history"
 export NPM_CONFIG_EDITOR="$EDITOR"
@@ -83,25 +86,20 @@ export NPM_CONFIG_PROGRESS='false'
 export NPM_CONFIG_SAVE='true'
 export NPM_CONFIG_CACHE_MIN='120'
 
-export bower_storage__packages="$XDG_CACHE_HOME/bower/packages"
-export bower_storage__registry="$XDG_CACHE_HOME/bower/registry"
-export bower_storage__links="$XDG_CACHE_HOME/bower/links"
-export bower_tmp="$XDG_CACHE_HOME/bower/tmp"
-
 export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/pythonrc"
 
 export SCREENRC="$XDG_CONFIG_HOME/screen/screenrc"
 
 export SNIPPETS_FOLDER="$XDG_CONFIG_HOME/snippets"
 
-export TIMEFMT=$'\e[33m%J\e[0m: \e[35m%*E\e[0m'
-
 export TIGRC_USER="$XDG_CONFIG_HOME/tig/tigrc"
 
-export VIMPAGER_RC="$XDG_CONFIG_HOME/vimpager/vimpagerrc"
-
-export ZIPOPT='-9'
+export TIMEFMT=$'\e[33m%J\e[0m: \e[35m%*E\e[0m'
 
 export VAGRANT_CHECKPOINT_DISABLE='yes'
 
+export VIMPAGER_RC="$XDG_CONFIG_HOME/vimpager/vimpagerrc"
+
 export WWW_HOME='https://duckduckgo.com'
+
+export ZIPOPT='-9'
