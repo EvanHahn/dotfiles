ffbd0cd039a21f0d52d3b57cdc06f76009ae5cd5
Evan Hahn <me@evanhahn.com>
2017-02-01 17:55:42 -0800

    vimplug installer should set a default dir for XDG_CONFIG_HOME

--- script/install-vim-plug.sh
+++ script/install-vim-plug.sh
@@ -3,5 +3,7 @@ set -e
 set -u
 set -o pipefail
 
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+
 curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo "$XDG_CONFIG_HOME/nvim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
