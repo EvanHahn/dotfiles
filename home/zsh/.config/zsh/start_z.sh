e680a751538e1ed74fa0a0987729aba4d5d6efac
Evan Hahn <me@evanhahn.com>
2017-07-25 11:01:03 -0500

    zsh: clean up start_z script

--- home/zsh/.config/zsh/start_z.sh
+++ home/zsh/.config/zsh/start_z.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/env bash
-set -eu
-
 if [[ "$(uname -s)" = 'Darwin' ]]; then
   z_path="$(brew --prefix)/etc/profile.d/z.sh"
 else
@@ -8,6 +5,7 @@ else
 fi
 
 if [[ -e "$z_path" ]]; then
-  set +eu
   source "$z_path"
 fi
+
+unset z_path
