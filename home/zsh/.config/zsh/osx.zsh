a2ed4d9a733e53c21fa603a3caf0db80f89ae30b
Evan Hahn <me@evanhahn.com>
2018-03-14 11:52:59 -0500

    macOS: fix `gvim` alias

--- home/zsh/.config/zsh/osx.zsh
+++ home/zsh/.config/zsh/osx.zsh
@@ -11,4 +11,4 @@ unset DISPLAY
 
 sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent' # flush downloads list
 
-alias gvim=mvim
+alias gvim='open -a MacVim'
