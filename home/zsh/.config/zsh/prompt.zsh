12d34f9db6d9ea3472a716036c9633a3fdb245fc
Evan Hahn <me@evanhahn.com>
2019-01-18 13:01:51 -1000

    zsh: big ol line before the prompt

--- home/zsh/.config/zsh/prompt.zsh
+++ home/zsh/.config/zsh/prompt.zsh
@@ -10,19 +10,35 @@ for color in {000..255}; do
 done
 unset color
 
+_prompt_hr() {
+  local width
+  local color
+
+  width="$(echo "$(stty size)" | cut -d ' ' -f 2)"
+  color="$((RANDOM % 5 + 1))"
+
+  tput setaf "$color"
+  tput setab "$color"
+  seq -f '-' -s '' "$width"
+
+  tput sgr0
+}
+
 _prompt_status_color() {
   if [[ "$?" == '0' ]]; then
     if [[ -n "$SSH_CONNECTION" ]]; then
-      echo -e "$FG[117]"
+      tput setaf 6
     else
-      echo -e "$FG[239]"
+      tput sgr0
     fi
   else
-    echo -e "$FG[124]"
+    tput setaf 1
   fi
 }
 
 PROMPT='
+%{$(_prompt_hr)%}
+
  %{$(_prompt_status_color)%}>%{$reset_color%} '
 
 RPROMPT='%1(j.(%j).) %{$(_prompt_status_color)%}%~%u%{$reset_color%}'
