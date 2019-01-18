9824b645cf9ca22c9f64e4de0840b4c740af8fe1
Evan Hahn <me@evanhahn.com>
2019-01-18 13:02:50 -1000

    zsh: remove $FG and $BG from prompt script

--- home/zsh/.config/zsh/prompt.zsh
+++ home/zsh/.config/zsh/prompt.zsh
@@ -3,13 +3,6 @@ autoload colors; colors # enable colors
 setopt prompt_subst # enable hecka stuff
 setopt transient_rprompt # show rprompt on current prompt only
 
-typeset -Ag FG BG
-for color in {000..255}; do
-	FG[$color]="[38;5;${color}m"
-	BG[$color]="[48;5;${color}m"
-done
-unset color
-
 _prompt_hr() {
   local width
   local color
