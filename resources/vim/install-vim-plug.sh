85a3943295ba131629c5e0fe0de768d5fd4b974e
Evan Hahn <me@evanhahn.com>
2019-03-07 12:11:47 -0600

    vim: put everything in neovim config directories

--- resources/vim/install-vim-plug.sh
+++ resources/vim/install-vim-plug.sh
@@ -5,5 +5,8 @@ set -o pipefail
 
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 
-curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -fLo "$XDG_CONFIG_HOME/nvim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' \
+  --fail \
+  --location \
+  --output "$XDG_CONFIG_HOME/nvim/autoload/plug.vim" \
+  --create-dirs
