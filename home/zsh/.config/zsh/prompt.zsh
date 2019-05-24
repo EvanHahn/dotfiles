fd87f9637a427cd882accec9f4aabca64f720c7e
Evan Hahn <me@evanhahn.com>
2019-05-24 16:07:01 -0500

    tmux and zsh: improve coloring when inside SSH

--- home/zsh/.config/zsh/prompt.zsh
+++ home/zsh/.config/zsh/prompt.zsh
@@ -3,21 +3,26 @@ autoload colors; colors # enable colors
 setopt prompt_subst # enable hecka stuff
 setopt transient_rprompt # show rprompt on current prompt only
 
+_prompt_color() {
+  if [[ -n "$SSH_CONNECTION" ]]; then
+    tput setaf 6
+  else
+    tput sgr0
+  fi
+}
+
 _prompt_hr() {
   local width
 
   width="$(echo "$(stty size)" | cut -d ' ' -f 2)"
 
+  _prompt_color
   printf '–%.0s' {1..$width}
 }
 
 _prompt_status_color() {
   if [[ "$?" == '0' ]]; then
-    if [[ -n "$SSH_CONNECTION" ]]; then
-      tput setaf 6
-    else
-      tput sgr0
-    fi
+    _prompt_color
   else
     tput setaf 1
   fi
