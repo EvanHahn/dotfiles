2347aea0a77f44b9ea9d74f3297dafbf9d4dfb9c
Evan Hahn <me@evanhahn.com>
2020-12-21 20:25:43 -0600

    zsh: set homebrew options on all platforms, not just macOS

--- home/zsh/.config/zsh/mac.zsh
+++ home/zsh/.config/zsh/mac.zsh
@@ -8,10 +8,3 @@ export XDG_VIDEOS_DIR="$HOME/Videos"
 
 export BROWSER=open
 unset DISPLAY
-
-export HOMEBREW_NO_EMOJI=1
-export HOMEBREW_NO_GITHUB_API=1
-export HOMEBREW_NO_ANALYTICS=1
-export HOMEBREW_NO_AUTO_UPDATE=1
-export HOMEBREW_NO_INSECURE_REDIRECT=1
-export HOMEBREW_CASK_OPTS="--appdir='$HOME/Applications' --require-sha"
