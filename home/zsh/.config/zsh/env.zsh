799c32eb19de998de056a72c1d6242b567286d8b
Evan Hahn <me@evanhahn.com>
2022-06-24 20:58:53 -0500

    zsh: Homebrew on ARM

--- home/zsh/.config/zsh/env.zsh
+++ home/zsh/.config/zsh/env.zsh
@@ -11,6 +11,8 @@ prepend() {
   [ -d "$1" ] && PATH="$1:$PATH"
 }
 prepend '/usr/local/bin'
+prepend '/opt/homebrew/bin'
+prepend '/opt/homebrew/sbin'
 prepend "$HOME/bin"
 prepend '/home/linuxbrew/.linuxbrew/bin'
 unset prepend
