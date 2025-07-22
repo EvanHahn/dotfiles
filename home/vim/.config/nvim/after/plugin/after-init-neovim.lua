f254b43c8abf706a812df2f15626dd34b72c339f
Evan Hahn <me@evanhahn.com>
2025-07-21 19:32:12 -0500

    vim: `thesaurusfunc`
    I grabbed a thesaurus from [here][0], parsed it, and now I can run
    <C-X><C-T> to get synonyms.
    
    [0]: https://wiki.openoffice.org/wiki/Dictionaries

--- home/vim/.config/nvim/after/plugin/after-init-neovim.lua
+++ home/vim/.config/nvim/after/plugin/after-init-neovim.lua
@@ -1,5 +1,51 @@
 ------------------------------------------------------------------------------
 --
+-- Thesaurus support. <C-X><C-T> in Insert mode finds synonyms.
+--
+-- This basically just reads a thesaurus file. It works a lot like the
+-- `thesaurus` option but it supports multi-word synonyms. It could be much
+-- more elegant, but this kludge is fast enough for me!
+--
+-- I *could* implement this in vanilla Vim but I don't think it's worth the
+-- effort.
+--
+------------------------------------------------------------------------------
+
+local thes_path = vim.fn.expand('$HOME/.cache/evanhahn-vim-thesaurus')
+
+function evanhahn_thesaurusfunc(findstart, base)
+  -- Handle the first invocation of this function. Lifted from [here][0],
+  -- licensed under the MIT License.
+  -- [0]: https://github.com/potamides/dotfiles/blob/fae13899e28c17b5c044bb3932687428aa43eeb1/.config/nvim/lua/mythes.lua#L59-L62
+  if findstart == 1 then
+    local line, col = vim.api.nvim_get_current_line(), vim.api.nvim_win_get_cursor(0)[2]
+    return vim.fn.match(line:sub(1, col), '\\k*$')
+  end
+
+  local result = {}
+
+  local thes_file = io.open(thes_path)
+  if not thes_file then
+    error("Couldn't open thesaurus file. It might not exist; see README instructions.")
+  end
+
+  for line in thes_file:lines('l') do
+    local entry_and_synonyms = vim.split(line, ",")
+    if entry_and_synonyms[1] == base then
+      result = entry_and_synonyms
+    end
+  end
+
+  thes_file:close()
+
+  return result
+end
+
+vim.opt.thesaurus = ''
+vim.opt.thesaurusfunc = 'v:lua.evanhahn_thesaurusfunc'
+
+------------------------------------------------------------------------------
+--
 -- LSP setup.
 --
 ------------------------------------------------------------------------------
